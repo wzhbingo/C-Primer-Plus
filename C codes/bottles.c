7e37aec9a0702a266a05ec84872a336e606fe014
wzhbingo <475636268@qq.com>
2018-08-19 10:03:24 +0800

    codes

---
+++ C codes/bottles.c
@@ -0,0 +1,15 @@
+// bottles.c
+#include <stdio.h>
+#define MAX 100
+int main(void){
+	int count = MAX + 1;
+	
+	while(--count >0){
+		printf("%d bottles of sprint water on the wall, "
+		"%d bottles of sprint water!\n", count, count);
+		printf("Take one down and pass it around,\n");
+		printf("%d bottles of spinrt water!\n\n", count - 1);
+	}
+	
+	return;
+}
